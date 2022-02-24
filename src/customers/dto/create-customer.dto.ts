b98fe12be98c3d164fb48d1d0daafa3e46ec66d5
Aneli Valdés <aneli.valdes@thoughtworks.com>
2022-02-24 17:47:28 -0300

    Add | swagger

--- src/customers/dto/create-customer.dto.ts
+++ src/customers/dto/create-customer.dto.ts
@@ -1,6 +1,9 @@
 import { Model } from 'sequelize';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateCustomerDto extends Model {
+  @ApiProperty()
   firstName: string;
+  @ApiProperty()
   lastName: string;
 }
