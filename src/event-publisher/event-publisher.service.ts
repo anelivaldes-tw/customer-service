642d9a66acf71406dd9ec76f7773e2c3f2b8d173
Aneli Valdés <aneli.valdes@thoughtworks.com>
2022-03-15 11:27:40 -0300

    fix | bug found

--- src/event-publisher/event-publisher.service.ts
+++ src/event-publisher/event-publisher.service.ts
@@ -8,6 +8,7 @@ export class EventPublisherService {
   client: ClientKafka;
 
   publish(event, topic = 'customer') {
+    // TODO: There is a bug here, sometime Producer is disconnected, and do not publish. subscribe to observable, to see.
     this.client.emit<string>(topic, event);
     console.log('Published:', event);
     console.log('To:', topic);
